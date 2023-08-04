9c62a610ea71381a1bdde1cb267a16b20f8010c5
Danae <bmil730@wgu.edu>
2023-08-04 14:08:57 -0400

    Part C.1.b. Adds controller and front end support to display new welcome messages using threads

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -1,15 +1,28 @@
 package edu.wgu.d387_sample_code;
 
+import edu.wgu.d387_sample_code.controllers.WelcomeMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.InputStream;
+import java.util.Locale;
+import java.util.Properties;
 
 @SpringBootApplication
 public class D387SampleCodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(D387SampleCodeApplication.class, args);
-	}
 
+		WelcomeMessage displayMessageEnglish = new WelcomeMessage(Locale.US);
+		WelcomeMessage displayMessageFrench = new WelcomeMessage(Locale.CANADA_FRENCH);
+		Thread englishThread = new Thread(displayMessageEnglish);
+		Thread frenchThread = new Thread(displayMessageFrench);
+		englishThread.start();
+		frenchThread.start();
+
+	}
 }
